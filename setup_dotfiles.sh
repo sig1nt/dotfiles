0f8a62bc1735dbbbfb6175c252f32c6dc913e837
atti1a <ngonella@calpoly.edu>
2017-05-31 23:38:08 -0700

    Update install script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,5 +1,17 @@
-#!/usr/local/bin/zsh
+#!/usr/bin/env zsh
+
+# Install Various Programs
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# Add custom theme
+ln -sf $PWD/zsh_theme ~/.oh_my_zsh/themes/custom.zsh-theme
+
+# Link in dotfiles
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/aliases ~/.aliases
 ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf $PWD/tmux_conf ~/.tmux_conf
+
 source ~/.zshrc
