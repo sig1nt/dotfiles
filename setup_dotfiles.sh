0607d22d9b8c19ae5057c21ec57afde5d4983e7a
atti1a <ngonella@calpoly.edu>
2017-05-31 23:49:29 -0700

    Remove GPG by default in git

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -5,7 +5,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # Add custom theme
-ln -sf $PWD/zsh_theme ~/.oh_my_zsh/themes/custom.zsh-theme
+ln -sf $PWD/zsh_theme ~/.oh-my-zsh/themes/custom.zsh-theme
+
+# Setup Vim Folder
+mkdir -p ~/.vim/{swp,backup}
 
 # Link in dotfiles
 ln -sf $PWD/zshrc ~/.zshrc
